05b23e231de16f920f37519f8ef0dcdfb506be3c
Yuanheng Li <foureverhh@Yuanhengs-MacBook-Air.local>
2018-01-10 18:19:34 +0100

    Initial commit implements RadioGroup.onCheckChangeListener,TextWatcher setOnCheckedChangeListener(this) addTextChangedListener(this)

---
+++ app/src/main/java/com/nackademin/foureverhh/temperatureconverter180110_1/MainActivity.java
@@ -0,0 +1,75 @@
+package com.nackademin.foureverhh.temperatureconverter180110_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener,TextWatcher {
+
+    RadioGroup unit;
+    EditText value;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        unit = (RadioGroup)findViewById(R.id.unit);
+        unit.setOnCheckedChangeListener(this);
+
+        value = (EditText)findViewById(R.id.value);
+        value.addTextChangedListener(this);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        calculate();
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        calculate();
+    }
+
+    protected void calculate()
+    {
+        TextView degF =(TextView)findViewById(R.id.degF);
+        TextView degC =(TextView)findViewById(R.id.degC);
+
+        double f=0,c=0;
+
+        if(unit.getCheckedRadioButtonId()==R.id.unitF)
+        {
+            f=Double.parseDouble(value.getText().toString());
+            c=(f-32)*5/9;
+        }else if(unit.getCheckedRadioButtonId()==R.id.unitC)
+        {
+            c=Double.parseDouble(value.getText().toString());
+            f=c*9/5+32;
+        }
+
+        if(value.length()==0)
+        {
+            value.setText("You must input a number here!");
+        }else
+        {
+            degF.setText(String.format("%.1f",c)+" "+getResources().getString(R.string.charC));
+            degC.setText(String.format("%.1f",f)+" "+getResources().getString(R.string.charF));
+        }
+    }
+}
